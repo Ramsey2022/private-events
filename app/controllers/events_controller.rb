1947b8dd4ef498e9ebfa960179aa44a599dbe927
Ramsey2022 <abovethelotus@yahoo.com>
2023-07-17 13:00:51 -0700

    added more event functions

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,49 @@
 class EventsController < ApplicationController
   def index
+    @events = Event.all
   end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = current_user.created_events.build(event_params)
+    if @event.save
+      redirect_to @event, notice: "Your event was created"
+    else
+      render :new
+    end
+  end
+
+  def show
+    @event = Event.find(params[:id])
+  end
+
+  def edit
+    @event = Event.find(params[:id])
+  end
+
+  def update
+    @event = Event.find(params[:id])
+    if @event.update(event_params)
+      redirect_to @event, notice: "Your event was updated"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @event = Event.find(params[:id])
+    @event.destroy
+    redirect_to events_path
+  end
+
+
+  private
+
+  def event_params
+    params.require(:event).permit(:title, :description, :date, :time, :location)
+  end
+
 end
